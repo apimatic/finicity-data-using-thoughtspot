bd7ca21367c5349c1c4a836f02eb8777c3bbb318
Wajahat Iqbal <wajahat.iqbal@apimatic.io>
2022-08-26 15:21:40 +0500

    add comments for where to insert the code

--- server/src/finicity.ts
+++ server/src/finicity.ts
@@ -1,7 +1,6 @@
 import cors from 'cors';
 import dotenv from 'dotenv'
 import express, { Request, Response } from 'express';
-import { AuthenticationController, Client, PartnerCredentials, TransactionsController } from 'finicity-apilib';
 import { getConnection } from './connection';
 import { insertquery } from './jsonToSql';
 
@@ -16,39 +15,40 @@ router.get('/finicitydata', finicityController);
 
 async function finicityController(req: Request, resp: Response) {
 
+  // add code to call get transactions all endpoint
 
-    const autoQuery = insertquery(result);
+  const autoQuery = insertquery(result);
 
-    //create snowflake connection
-    var connection = getConnection();      
-    console.log("connectionId is: ", connection.getId());
+  //create snowflake connection
+  var connection = getConnection();      
+  console.log("connectionId is: ", connection.getId());
 
-    connection.execute(
-    {
-      sqlText: `DELETE FROM TRANSACDATA`,
-      streamResult: true,
-      complete: function (_err, stmt, rows) {
-        rows = [];
-        stmt.streamRows(
-          //   {
-          //   start: 0,
-          //   end: 10,
-          // }
-        )
-          .on('error', function (err) {
-            console.error('Unable to consume requested rows');
-          })
-          .on('data', function (row) {
-            if (typeof rows !== 'undefined')
-              rows.push(row);
-            console.log("rows: ", rows);
-          })
-          .on('end', function () {
-            if (typeof rows !== 'undefined')
-              console.log('Number of rows consumed for delete : ' + rows.length);
-          });
-      }
-    });
+  connection.execute(
+  {
+    sqlText: `DELETE FROM TRANSACDATA`,
+    streamResult: true,
+    complete: function (_err, stmt, rows) {
+      rows = [];
+      stmt.streamRows(
+        //   {
+        //   start: 0,
+        //   end: 10,
+        // }
+      )
+        .on('error', function (err) {
+          console.error('Unable to consume requested rows');
+        })
+        .on('data', function (row) {
+          if (typeof rows !== 'undefined')
+            rows.push(row);
+          console.log("rows: ", rows);
+        })
+        .on('end', function () {
+          if (typeof rows !== 'undefined')
+            console.log('Number of rows consumed for delete : ' + rows.length);
+        });
+    }
+  });
       
   connection.execute(
     {
@@ -79,12 +79,15 @@ async function finicityController(req: Request, resp: Response) {
     });
 }
 
-async function getAccessToken() {
 
+// add code to get access token
+async function getAccessToken() {
 }
 
+// add code to get tokenized client
 async function createTokenizedClient(client: Client) {
 }
 
+// add code to create transactions controller with initialized client
 async function createTransactionController(client: Client) {
 }
